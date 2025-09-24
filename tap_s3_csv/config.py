57db9d2c6c805dfdd06a3c71ed46039d52f41d1b
moses <60942157+vemqar@users.noreply.github.com>
2025-09-24 17:54:50 -0400

    initial

--- tap_s3_csv/config.py
+++ tap_s3_csv/config.py
@@ -6,23 +6,20 @@ from __future__ import annotations
 
 from voluptuous import Optional, Required, Schema
 
-CONFIG_CONTRACT = Schema(
-    [
-        {
-            Required("table_name"): str,
-            Required("search_pattern"): str,
-            Optional("key_properties"): [str],
-            Optional("search_prefix"): str,
-            Optional("date_overrides"): [str],
-            Optional("string_overrides"): [str],
-            Optional("datatype_overrides"): object,
-            Optional("guess_types"): bool,
-            Optional("delimiter"): str,
-            Optional("table_suffix"): str,
-            Optional("remove_character"): str,
-            Optional("s3_proxies"): object,
-            Optional("encoding"): str,
-            Optional("set_empty_values_null"): bool,
-        }
-    ]
-)
+CONFIG_CONTRACT = Schema({
+    Required("table_name"): str,
+    Required("search_pattern"): str,
+    Optional("key_properties"): [str],
+    Optional("search_prefix"): str,
+    Optional("date_overrides"): [str],
+    Optional("string_overrides"): [str],
+    Optional("datatype_overrides"): object,
+    Optional("guess_types"): bool,
+    Optional("delimiter"): str,
+    Optional("table_suffix"): str,
+    Optional("remove_character"): str,
+    Optional("s3_proxies"): object,
+    Optional("encoding"): str,
+    Optional("set_empty_values_null"): bool,
+    Optional("escapechar"): str,
+})
